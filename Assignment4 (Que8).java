776fc5a4290cf2ffa5ecc8a6bbd6370a5d944a7f
Abdi <113378353+AbdirahmanShafi@users.noreply.github.com>
2023-07-20 11:09:20 +0300

    Add files via upload

---
+++ Assignment4 (Que8).java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment4;
+
+/**
+ *
+ * @author abdir
+ */
+public class Assignment4Q8 {
+    
+   public static void main(String[] arg) {
+
+        int num = 12;
+        
+        System.out.println("Even numbers from 12 to 50:");
+        do{
+            System.out.println(num +"");
+            num +=2;
+        }while(num<=50);
+    }
+}
